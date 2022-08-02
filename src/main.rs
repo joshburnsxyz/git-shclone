3ac4b7dc2cc58e6e2870494fd820fd355cc9272d
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-08-03 07:45:42 +1000

    comments

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,22 @@ fn main() {
     let matches = command!()
         .arg(arg!([REPO]))
         .arg(arg!([DEST]).default_value(&format!("./")))
+        .arg(arg!(-h --host [HOST], "One of: github, gitlab"]))
         .get_matches();
+    
     let repo_url = format!("git@github.com:{}", matches.get_one::<String>("REPO").unwrap());
     let dest_path = format!("./{}", matches.get_one::<String>("REPO").unwrap().split("/").next().unwrap());
+    let _git_host = matches.get_one::<String>("HOST").unwrap();
+
     let output = Command::new("git")
         .args(["clone", &repo_url, &dest_path])
         .output()
         .expect("fail");
+
+    // Write command output to respective IO streams
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
+
+    // Exit with code zero
     exit(0);
 }
